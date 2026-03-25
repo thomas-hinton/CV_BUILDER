085d6fcea6ee74895ac971220ecf1766d2457b94
UlyssouLV <ulysse1304@gmail.com>
2026-03-25 16:40:59 +0100

    index & admin html structure basic

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 # My app
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.responses import FileResponse
+from pathlib import Path
 
 app = FastAPI()
 
 coffees = []
 
+BASE_DIR = Path(__file__).resolve().parent
+
 @app.get("/")
 def home():
-    return "Bienvenue !"
+    return FileResponse(BASE_DIR / "index.html")
 
+@app.get("/admin")
+def admin():
+    return FileResponse(BASE_DIR / "admin.html")
